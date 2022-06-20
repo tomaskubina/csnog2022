1828c2ac29e8a5d7bf46c4742c76c4d19124176e
Tomas Kubina <tomas.kubina@orange.com>
2022-06-20 14:45:20 +0200

    update of script

--- gnmi_examples/get_full_config.py
+++ gnmi_examples/get_full_config.py
@@ -3,6 +3,6 @@ import json
 
 host = ('192.168.243.21', '50001')
 
-with gNMIclient(target=host, username='nsouser', password='Welcome41', insecure=True, debug=0) as gc:
+with gNMIclient(target=host, username='username', password='password', insecure=True, debug=0) as gc:
     result = gc.get(path=[], encoding='json_ietf', datatype='config')
     print(json.dumps(result, indent=4))
